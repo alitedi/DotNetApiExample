30cfef3a340c5ed95017393ad0b28fdd6fa8df0e
ali <alireza.rtbd@gmail.com>
2023-03-04 20:02:38 +0100

    CORS : Cross Origin Resource Sharing

--- DotNetApi/Program.cs
+++ DotNetApi/Program.cs
@@ -3,9 +3,23 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+// CORS : Cross Origin Resource Sharing 
+// Access to API throw out of server 
+// From applications that are deployed to machines that are not on the same resource as our API. 
+builder.Services.AddCors(options =>
+{
+    // Allow all to access to all the resources 
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyHeader()
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod());
+});
 
 var app = builder.Build();
 
@@ -17,6 +31,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
