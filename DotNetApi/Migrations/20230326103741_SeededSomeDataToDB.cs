076f06fcf32c71d95fecf5346042eeb6214d0f71
ali <alireza.rtbd@gmail.com>
2023-03-26 12:42:41 +0200

    Seeded some data to DB

---
+++ DotNetApi/Migrations/20230326103741_SeededSomeDataToDB.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DotNetApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeededSomeDataToDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Poets",
+                columns: new[] { "Id", "BirthDay", "Name" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(1355, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Hafez" },
+                    { 2, new DateTime(1207, 9, 30, 0, 0, 0, 0, DateTimeKind.Unspecified), "Molana" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Poetries",
+                columns: new[] { "Id", "Description", "Name", "PoetId" },
+                values: new object[] { 1, "مثنوی معنوی", "Masnavi manavi", 2 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Poetries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Poets",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Poets",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
--- DotNetApi/Models/PoetryDBContext.cs
+++ DotNetApi/Models/PoetryDBContext.cs
@@ -16,6 +16,27 @@ namespace DotNetApi.Models
         {
             base.OnModelCreating(modelBuilder);
             // Set some default data 
+            modelBuilder.Entity<Poet>().HasData(
+                new Poet()
+                {
+                    Name = "Hafez",
+                    BirthDay = new DateTime(1355, 1, 1),
+                    Id = 1
+                }, new Poet()
+                {
+                    Name = "Molana",
+                    BirthDay = new DateTime(1207, 9, 30),
+                    Id = 2
+                });
+
+            modelBuilder.Entity<Poetry>().HasData(
+                new Poetry()
+                {
+                    Name = "Masnavi manavi",
+                    PoetId = 2,
+                    Description = "مثنوی معنوی",
+                    Id = 1
+                });
         }
     }
 }
